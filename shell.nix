2848e83b2a59b78951a19efa66f1d30697511535
José Romildo Malaquias <malaquias@gmail.com>
2019-05-27 13:05:34 -0300

    shell.nix: use latest ocaml

--- shell.nix
+++ shell.nix
@@ -2,8 +2,7 @@
 
 let
   inherit (nixpkgs) pkgs;
-  #ocamlPackages = pkgs.ocamlPackages_latest;
-  ocamlPackages = pkgs.ocamlPackages;
+  ocamlPackages = pkgs.ocamlPackages_latest;
 in
 
 pkgs.stdenv.mkDerivation {
