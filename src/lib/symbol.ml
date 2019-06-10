778952256a0592693fde831c8767bd659b326dc1
José Romildo Malaquias <malaquias@gmail.com>
2019-06-10 13:41:32 -0300

    Add Symbol

---
+++ src/lib/symbol.ml
@@ -0,0 +1,42 @@
+(* symbol.ml *)
+
+type symbol = string * int
+[@@deriving show]
+
+let nextsym = ref 0
+
+let size_hint = 128
+
+let hashtable = Hashtbl.create size_hint
+
+let name (s,n) = s
+
+let symbol name =
+  try
+    let i = Hashtbl.find hashtable name in
+    (name,i)
+  with Not_found ->
+    let i = !nextsym in
+    nextsym := i + 1;
+    Hashtbl.add hashtable name i;
+    (name,i)
+
+let new_symbol s = symbol (Printf.sprintf "%s_%d" s !nextsym)
+
+
+module Table = Map.Make(
+  (* purely applicative (no side-effects) association tables over ordered types *)
+  struct
+    type t = symbol
+    let compare (_,n1) (_,n2) = compare n1 n2
+  end
+  )
+
+type 'a table = 'a Table.t
+
+let empty = Table.empty
+let enter = Table.add
+(* let look = Table.find *)
+let look k t =
+  try Some (Table.find k t)
+  with Not_found -> None
